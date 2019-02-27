1b79827d5df97155be98a1adc9edbef02053f33b
yuhsuan <x89301089@gmail.com>
2019-02-27 11:30:07 +0800

    #1 [add] scroll div

--- src/app/sale-record/sale-record.component.ts
+++ src/app/sale-record/sale-record.component.ts
@@ -3,7 +3,7 @@ import { Time } from '@angular/common';
 
 
 interface OrderList {
-  number:any,
+  number: any,
   day: any,
   time: any
 }
@@ -36,91 +36,121 @@ export class SaleRecordComponent implements OnInit {
   ngOnInit() {
     this.listArray = [
       {
-        number:'01.',
+        number: '01.',
         day: '2016-12-01',
         time: '11:30:03'
       },
       {
-        number:'02.',
+        number: '02.',
         day: '2016-12-02',
         time: '11:30:03'
       },
       {
-        number:'03.',
+        number: '03.',
         day: '2016-12-03',
         time: '11:30:03'
       },
       {
-        number:'04.',
+        number: '04.',
         day: '2016-12-04',
         time: '11:30:03'
       },
       {
-        number:'05.',
+        number: '05.',
         day: '2016-12-05',
         time: '11:30:03'
       },
       {
-        number:'06.',
+        number: '06.',
         day: '2016-12-06',
         time: '11:30:03'
       },
       {
-        number:'07.',
+        number: '07.',
         day: '2016-12-07',
         time: '11:30:03'
       },
       {
-        number:'08.',
+        number: '08.',
         day: '2016-12-08',
         time: '11:30:03'
       },
       {
-        number:'09.',
+        number: '08.',
+        day: '2016-12-08',
+        time: '11:30:03'
+      },
+      {
+        number: '08.',
+        day: '2016-12-08',
+        time: '11:30:03'
+      },
+      {
+        number: '08.',
+        day: '2016-12-08',
+        time: '11:30:03'
+      },
+      {
+        number: '09.',
         day: '2016-12-09',
         time: '11:30:03'
       }
     ]
-    this.totalArray=[
+    this.totalArray = [
       {
-        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸..',
+        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸',
         quantity: 10,
-        price:140000
+        price: 140000
       },
       {
-        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸..',
+        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸',
         quantity: 9,
-        price:140
+        price: 140
       },
       {
-        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸..',
+        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸',
         quantity: 8,
-        price:140000
+        price: 140000
       },
       {
-        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸..',
+        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸',
+        quantity: 8,
+        price: 140000
+      },
+      {
+        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸',
+        quantity: 8,
+        price: 140000
+      },
+      {
+        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸',
+        quantity: 8,
+        price: 140000
+      },
+      {
+        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸',
         quantity: 7,
-        price:140000
+        price: 140000
       },
       {
-        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸..',
+        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸',
         quantity: 6,
-        price:140000
+        price: 140000
       },
       {
-        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸..',
+        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸',
         quantity: 5,
-        price:140000
+        price: 140000
       },
       {
-        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸..',
+        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸',
         quantity: 4,
-        price:140000
+        price: 140000
       },
       {
-        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸..',
+        name: '羅勒風味海瓜子蛤蜊炒蝦仁意麵腸',
         quantity: 3,
-        price:140000
+        price: 140000
       }
     ]
   }
